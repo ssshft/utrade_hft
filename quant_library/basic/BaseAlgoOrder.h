3b96dba77fd6fcfd39ade4957e0c826ed432b753
lawson <lawson@192.168.1.5>
2026-09-20 20:54:01 +0800

    update

--- quant_library/basic/BaseAlgoOrder.h
+++ quant_library/basic/BaseAlgoOrder.h
@@ -26,7 +26,7 @@ struct BaseAlgoOrder {
     int64_t algoOrderId{0};
     char pairInstrumentKey[stra::INST_KEY_LEN]{""};
     char baseAsset[stra::ASSET_LEN]{""};
-    AlgoOrderStatus algoOrderStatus{stra::ALGO_OS_MIN};
+    stra::AlgoOrderStatus algoOrderStatus{stra::ALGO_OS_MIN};
 
     char activeInstrumentKey[stra::INST_KEY_LEN]{""};
     int activeAccountId{0};
