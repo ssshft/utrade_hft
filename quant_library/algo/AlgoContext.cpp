6d722fce2fd5738f45969129537ae4764c32d473
lawson <lawson@lawsondeMacBook-Pro.local>
2026-09-21 10:09:16 +0800

    update

--- quant_library/algo/AlgoContext.cpp
+++ quant_library/algo/AlgoContext.cpp
@@ -895,48 +895,48 @@ void AlgoContext::OnCommand(string s) {
     pAlgoOrder->ttOLStartSpread = -0.0001;
     pAlgoOrder->ttOLEndSpread = -0.0005;
     pAlgoOrder->ttOLStartVolume = 0.0;
-    pAlgoOrder->ttOLEndVolume = -300;
+    pAlgoOrder->ttOLEndVolume = -200;
     pAlgoOrder->ttOLSwitch = false;
       
     pAlgoOrder->ttCLStartSpread = 0;
     pAlgoOrder->ttCLEndSpread = 0.001;
-    pAlgoOrder->ttCLStartVolume = -300;
+    pAlgoOrder->ttCLStartVolume = -200;
     pAlgoOrder->ttCLEndVolume = 0;
     pAlgoOrder->ttCLSwitch = false;
     
     pAlgoOrder->ttOSStartSpread = 0;
-    pAlgoOrder->ttOSEndSpread = 0.001;
+    pAlgoOrder->ttOSEndSpread = 0.000001;
     pAlgoOrder->ttOSStartVolume = 0;
-    pAlgoOrder->ttOSEndVolume = 300;
+    pAlgoOrder->ttOSEndVolume = 200;
     pAlgoOrder->ttOSSwitch = true;
     
     pAlgoOrder->ttCSStartSpread = -0.0001;
     pAlgoOrder->ttCSEndSpread = -0.0005;
-    pAlgoOrder->ttCSStartVolume = 300;
+    pAlgoOrder->ttCSStartVolume = 200;
     pAlgoOrder->ttCSEndVolume = 0;
     pAlgoOrder->ttCSSwitch = false;
       
     pAlgoOrder->mtOLStartSpread = -0.0001;
     pAlgoOrder->mtOLEndSpread = -0.0005;
     pAlgoOrder->mtOLStartVolume = 0.0;
-    pAlgoOrder->mtOLEndVolume = -300;
+    pAlgoOrder->mtOLEndVolume = -200;
     pAlgoOrder->mtOLSwitch = false;
      
     pAlgoOrder->mtCLStartSpread = 0;
     pAlgoOrder->mtCLEndSpread = 0.001;
-    pAlgoOrder->mtCLStartVolume = -300;
+    pAlgoOrder->mtCLStartVolume = -200;
     pAlgoOrder->mtCLEndVolume = 0;
     pAlgoOrder->mtCLSwitch = false;
       
     pAlgoOrder->mtOSStartSpread = 0;
     pAlgoOrder->mtOSEndSpread = 0.001;
     pAlgoOrder->mtOSStartVolume = 0;
-    pAlgoOrder->mtOSEndVolume = 300;
+    pAlgoOrder->mtOSEndVolume = 200;
     pAlgoOrder->mtOSSwitch = false;
      
     pAlgoOrder->mtCSStartSpread = -0.0001;
     pAlgoOrder->mtCSEndSpread = -0.0005;
-    pAlgoOrder->mtCSStartVolume = 300;
+    pAlgoOrder->mtCSStartVolume = 200;
     pAlgoOrder->mtCSEndVolume = 0;
     pAlgoOrder->mtCSSwitch = false;
 
