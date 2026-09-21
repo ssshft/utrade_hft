6ca622575c7100ec03ba29666ff557e25a42c743
lawson <lawson@lawsondeMacBook-Pro.local>
2026-09-21 10:39:09 +0800

    update

--- quant_library/basic/PairManager.cpp
+++ quant_library/basic/PairManager.cpp
@@ -354,7 +354,7 @@ stra::QuantOrder PairOrder::CreatePassiveOrder(int64_t strategyOrderId, Position
         auto& leftAss = posMgr->GetAccount().mAsset[activeInfo.base];
         auto& pos = posMgr->GetAccount().mPosition[activeInstrumentKey];
         activeAmount += leftAss.totalAmount - leftAss.loanAmount;
-	//LOG_INFO("CreatePassiveOrder leftAss:%s activeAmount: %f leftAss.totalAmount: %f loanAmount: %f", activeInfo.instLeft.c_str(), activeAmount, leftAss.totalAmount, leftAss.loanAmount);
+	    LOG_INFO("CreatePassiveOrder leftAss:{} activeAmount:{} leftAss.totalAmount:{} loanAmount:{}", activeInfo.instLeft, activeAmount, leftAss.totalAmount, leftAss.loanAmount);
         if (activeInfo.calcType == 0) {
             if (pos.longPosition > stra::MIN_FLOAT) {
                 if (activeInstType == USDT_SWAP || activeInstType == USDT_FUTURES || activeInstType == BUSD_SWAP || activeInstType == C_SWAP || activeInstType == C_FUTURES) {
@@ -450,6 +450,8 @@ stra::QuantOrder PairOrder::CreatePassiveOrder(int64_t strategyOrderId, Position
             }
         }
     }
+
+    LOG_INFO("CreatePassiveOrder activeAmount:{} activeValue:{} passiveAmount:{} passiveValue:{}", activeAmount, activeInfo.value, passiveAmount, passiveInfo.value);
 
 
     // create order
